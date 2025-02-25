dfb3dcaa0bfa6c1df15e8ef2a5214df51aad2fd6
Sultan Faaiz <sultan.faaiz@yahoo.com>
2025-02-25 19:41:45 +0700

    Fix: Remove unnecessary anchor tag from menu item name in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,9 +71,7 @@
             <div class="max-w-sm max-h-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                 <img class="rounded-t-lg w-full max-h-32 object-cover" src="{{ asset('storage/'. $menu?->image) }}" alt="{{ ucfirst($menu->name) }} Image" />
                 <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ucfirst($menu->name)}}</h5>
-                    </a>
+                    <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ucfirst($menu->name)}}</h5>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Rp. {{ number_format($menu->price, 0, ',', '.') }}</p>
                     <a href="{{route('add-to-cart')}}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#108482] rounded-lg hover:opacity-50 add-to-cart" data-name="{{ucfirst($menu->name)}}" data-price="{{$menu->price}}">
                         Add
